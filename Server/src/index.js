e96420e7322bd06e049af41900a2ab4021f5990d
Maria Gabriela Sándigo Muñoz <137342274+MGSandigoMunoz@users.noreply.github.com>
2023-09-20 11:11:01 -0500

    Web_Server_Homework_Improved

--- Server/src/index.js
+++ Server/src/index.js
@@ -1,27 +1,43 @@
 const http = require ('http');//Esto se importa de nodeJS
-const characters= require('./utils/data.js') //Impoto data que va a ser mi servidor local
+
+const PORT = 3001;
+
+const characters= require('./utils/data.js'); //Impoto data que va a ser mi servidor local, es el archivo que tengo en la carpeta utils. Lo hago con require porque es nodeJS y en el archivo data se está exportando como un module.
+
+
 
 
 http.
 createServer((req,res) => {
 
-    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Origin', '*');//Es un corse que da permiso al front para acceder a la info que tengo en el backend
+
+//? RUTAS
+
+//Los console.log van a funcionar cuando haga un request en el navegador. Ej: refresh
 
     if(req.url.includes('/rickandmorty/character')){
 
-        const partid= req.url.split('/')//Parte URL en array
-        const idString= partid.pop()//Toma el último elemento de array
-        const id= Number(idString)
+        const partid= req.url.split('/')//Parte URL en array ["rickandmorty", "character", "id"]
+        const idString= partid.pop()//Toma el último elemento de array ["id"]
+        const id= Number(idString) 
 
-        const searchCharacter= characters.find(character => character.id === id)
+        const searchCharacter= characters.find(character => character.id === id) //Si no encuentra el character.id entonces devuleve undefine
 
-        if(searchCharacter){
-            res.writeHead (200, { "Content-Type":"application/json" })
-            res.end(JSON.stringify(searchCharacter))
+        if(searchCharacter){ //Importante el return en cada rest, para que no ejecute el resto del código.
+            return res
+                    .writeHead (200, { "Content-Type":"application/json" })//Este content tipe se da porquevoy a retornar un JSON
+                    .end(JSON.stringify (searchCharacter))//Convierto un objeto JavaScript en una cadena JSON
         } else {
-            res.writeHead(404, { "Content-Type":"application/json" })
-            res.end("Personaje no encontrado")
+            return res
+                    .writeHead(404, { "Content-Type":"application/json" })
+                    .end("Personaje no encontrado")
         }
 
      } 
-}).listen(3001,'localhost');
+     return res
+        .writeHead(404, { "Content-Type":"application/json" })
+        .end({message:"Lost in space.."})
+
+}).listen(PORT,'localhost');//Recibe el puerto y el host
+
